8e90e7cb8110cdea57ed0ad79bd6c8e1730668ea
Shimi66 <31352848+E-Voss66@users.noreply.github.com>
2017-09-20 09:54:48 -0700

    Lab 6 Problem 1 Day 2

--- lab_6/individual_lab/Problem1.java
+++ lab_6/individual_lab/Problem1.java
@@ -8,9 +8,12 @@ import kareltherobot.*;
 public class Problem1 extends Robot
 {
     
-    int[] beeperList = new int[7];
+    int[] beeperList = new int[8];
     
     int numberOfBeepers = 0;
+    
+    int rowNumber = 0;
+    
     
     public Problem1(int st, int av, Direction dir, int numBeepers) {
         super(st, av, dir, numBeepers);
@@ -18,10 +21,14 @@ public class Problem1 extends Robot
     
     public void sortBeepers() {
         moveToStart();
-        while(nextToABeeper()){
-            collectRow();
+         while(nextToABeeper()){
+            collectRow(rowNumber);
             moveToNextRow();
+            rowNumber++;
         }
+        
+        sortList();
+        moveToPutBeeper();
     }
     
     public void moveToStart() {
@@ -56,18 +63,92 @@ public class Problem1 extends Robot
         }
     }
     
-    public void collectRow(){
+    public void collectRow(int rowNumber){
         faceNorth();
         while(nextToABeeper()){
             pickBeeper();
             numberOfBeepers++;
             move();
         }
-        system.out.println(numberOfBeepers);
-        system.out.println(beeperList);
-        beeperList[0] = numberOfBeepers;
+        System.out.println(numberOfBeepers);
+        beeperList[rowNumber] = numberOfBeepers;
+        System.out.println(beeperList[rowNumber]);
         numberOfBeepers = 0;
+    }
+    
+    
+    public void moveToNextRow(){
+        faceSouth();
+        while(frontIsClear()){
+            move();
+        }
+        faceEast();
+        move();
+    }
+    
+    public void sortList(){
+        int x = 0;
+        int y = 1;
+        int placeHolder = 0;
+        while(!listSorted()){
+            if(beeperList[x] <= beeperList[y]) {
+                y++;
+                if (y > 7){
+                    x++;
+                    y = x + 1;
+                }
+            }
+            else {
+                placeHolder = beeperList[x];
+                beeperList[x] = beeperList[y];
+                beeperList[y] = placeHolder;
+            }
+            
+           
+        }
+            System.out.println(beeperList[0]);
+            System.out.println(beeperList[1]);
+            System.out.println(beeperList[2]);
+            System.out.println(beeperList[3]);
+            System.out.println(beeperList[4]);
+            System.out.println(beeperList[5]);
+            System.out.println(beeperList[6]);
+            System.out.println(beeperList[7]);
+            System.out.println(666);
+    }
+    
+    public boolean listSorted(){
+         if(beeperList[0] <= beeperList[1] && beeperList[1] <= beeperList[2]){
+            if(beeperList[2] <= beeperList[3] && beeperList[3] <= beeperList[4]){
+                if(beeperList[4] <= beeperList[5] && beeperList[5] <= beeperList[6]){
+                    if(beeperList[6] <= beeperList[7] ){
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                else {
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+        
+        
+    }
+    
+    public void moveToPutBeeper(){
+        
     }
    
 }
+
+
+
 
