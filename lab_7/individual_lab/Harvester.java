c02b9e71a1f3ee8491236cafeced1857719f6a95
Shimi66 <31352848+E-Voss66@users.noreply.github.com>
2017-09-25 11:57:47 -0700

    Lab 7 Problem 1 day 1

--- lab_7/individual_lab/Harvester.java
+++ lab_7/individual_lab/Harvester.java
@@ -9,16 +9,102 @@ public class Harvester extends Robot
 {
     int[][]field = new int[6][5];
     
+    int rowNumber = 0;
+    
+    int columnNumber = 0;
+    
     public Harvester(int st, int av, Direction dir, int numBeepers) {
         super(st, av, dir, numBeepers);
     }
     
     public void harvest() {
-        
+        pickUpFirstRow();
+        turn();
+        pickUpSecondRow();
+        reset();
+        for(int i = 0; i < 2; i++){
+            repeat();
+        }
     }
     
     public void printField() {
-       //Write instructions so that the field is printed to the console after harvesting
+       System.out.println(field);//Write instructions so that the field is printed to the console after harvesting
+    }
+    
+    public void faceNorth(){
+        while(!facingNorth()){
+            turnLeft();
+        }
+    }
+    
+    public void faceEast(){
+        while(!facingEast()){
+            turnLeft();
+        }
+    }
+    
+    public void faceWest(){
+        while(!facingWest()){
+            turnLeft();
+        }
+    }
+    
+    public void pickUpFirstRow(){
+        move();
+        for( int i = 0; i < 5; i++){
+            if(nextToABeeper()){
+                pickBeeper();
+                field[rowNumber][columnNumber] = 1;
+            }
+            else {
+                field[rowNumber][columnNumber] = 0;
+            }
+            move();
+            System.out.println(field[rowNumber][columnNumber]);
+            columnNumber ++;
+        }
+        System.out.println(666);
+        System.out.println(columnNumber);
+    }
+    
+    public void turn(){
+        faceNorth();
+        move();
+        rowNumber++;
+        faceWest();
+    }
+    
+    public void pickUpSecondRow(){
+        move();
+        System.out.println(100);
+        System.out.println(rowNumber);
+        System.out.println(columnNumber);
+        for( int i = 0; i < 5; i++){
+            
+            if(nextToABeeper()){
+                pickBeeper();
+                field[rowNumber][columnNumber-1] = 1;
+            }
+            else{
+                field[rowNumber][columnNumber] = 0;
+            }
+            columnNumber--;
+            move();
+        }
+    }
+    
+    public void reset(){
+        faceNorth();
+        move();
+        rowNumber++;
+        faceEast();
+    }
+    
+    public void repeat(){
+        pickUpFirstRow();
+        turn();
+        pickUpSecondRow();
+        reset();
     }
    
 }
